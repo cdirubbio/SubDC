761a5fc9718bd795d7b980a3db00adb91872255a
christian <cdirubbs@gmail.com>
2024-09-11 18:57:50 -0400

    demo ready

--- frontend/react-app/src/components/Listings/ListingCard/ListingCard.jsx
+++ frontend/react-app/src/components/Listings/ListingCard/ListingCard.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import "./ListingCard.css"
 import { Link } from "react-router-dom";
+import "./../../../images/listing.jpg"
 
 export default function ListingCard(props) {
     const link = "/listing/" + props.listing_id;
     return (
         <div className="ListingCard ">
-            <img src={props.listingImage} alt="Listing" className="listing-image" />
+            <img src="./../../images/listing.jpg" alt="Listing" className="listing-image" />
+            {/* <img src={props.listingImage} alt="Listing" className="listing-image" /> */}
             <h3>{props.listingName}</h3>
             <p><strong>${props.listingPrice}</strong>/month</p>
             <Link to={link}><button className="view-details-button">View Details</button></Link>
