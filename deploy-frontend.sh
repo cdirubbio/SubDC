6b9a84c82498e78d454b69b2318e8f523ef456c1
christian <cdirubbs@gmail.com>
2024-09-27 14:32:45 -0400

    fix script

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -4,6 +4,6 @@ cd frontend/react-app
 npm install
 npm run build
 aws s3 rm s3://subdc.christiandirubbio.com --recursive
-aws s3 cp ./build/ s3://subdc.christiandirubbio.com --recursive
+aws s3 cp /home/ec2-user/subdc/frontend/react-app/build/ s3://subdc.christiandirubbio.com --recursive
 echo "Frontend deployed to S3"
 
