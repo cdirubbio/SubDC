4c284ce9e2f430e9b1f751a68ef30b9825e57e95
christian <cdirubbs@gmail.com>
2024-09-27 16:39:15 -0400

    give up on creating my own build server and just use managed option

--- backend/subdc-backend/server.js
+++ backend/subdc-backend/server.js
@@ -10,7 +10,6 @@ const favorites = require('./routes/favorites');
 const userStuff = require('./routes/userStuff');
 const verifyEmail = require('./routes/verify-email');
 const createListing = require('./routes/createListing');
-const webhook = require('./githubWebhook/webhook');
 
 const db = require('./db');
 
@@ -21,8 +20,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const githubWebhookServer = express();
-const webhookServerPort = process.env.WEBHOOK_SERVER_PORT
-githubWebhookServer.use(bodyParser.json());
 // Middleware thingy
 app.use(bodyParser.json());
 
@@ -59,9 +56,3 @@ app.use('/api', verifyEmail);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend Server is running on http://localhost:${PORT}`);
 });
-
-githubWebhookServer.use('/api', webhook);
-
-githubWebhookServer.listen(webhookServerPort, "0.0.0.0", () => {
-  console.log(`GitHub Webhook Listener server is running on http://localhost:${webhookServerPort}`)
-})
