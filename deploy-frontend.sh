60c88d100363b5459613a5832996d7c45d825b5a
christian <cdirubbs@gmail.com>
2024-09-27 14:26:31 -0400

    final final

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 git pull origin main
 cd frontend/react-app
 npm install
 npm run build
-aws s3 sync ./build/ s3://subdc.christiandirubbio.com --delete
+aws s3 rm s3://subdc.christiandirubbio.com --recursive
+aws s3 cp ./build/ s3://subdc.christiandirubbio.com --recursive
 echo "Frontend deployed to S3"
 
