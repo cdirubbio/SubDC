aa5494e2c89d8f813845a3a20c15071704e816eb
christian <cdirubbs@gmail.com>
2024-10-24 22:19:04 -0400

    view fixes

--- frontend/react-app/src/components/AboutUs/AboutUs.jsx
+++ frontend/react-app/src/components/AboutUs/AboutUs.jsx
@@ -16,16 +16,16 @@ export default function AboutUs() {
     <div className="AboutUs flex flex-col items-center justify-center p-10">
       {/* Header */}
       <div className="text-left w-full mb-6">
-        <h2 className="text-black-400 text-md mb-2">About Us</h2>
+        <h2 className="aboutus-title text-black-400 text-md mb-2">About Us</h2>
     </div>
 
       <div className="AboutUs flex flex-col lg:flex-row items-center justify-center p-10">
         {/* Main Text Section */}
         <div className="text-section lg:w-1/2 flex flex-col items-center justify-center text-center mt-6 lg:mt-0 lg:text-left">
-            <h1 className="text-4xl font-bold">
+            <h1 className="aboutus text-4xl font-bold">
             We aim to provide DMV students with a safe and reliable method for university housing.
             </h1>
-            <p className="mt-5">
+            <p className="aboutus mt-5">
             Created on the GWU campus, our student founders discovered a need for a safe alternative to subletting. 
             Deterred by the prolific scams and shady business deals of our fellow classmates, we created a platform
             where students can trust that their housing needs will be taken seriously.
@@ -37,28 +37,30 @@ export default function AboutUs() {
         <img
         src={GWAboutImg}
         alt="Meet the SubDC Team."
-        className="rounded-xl shadow-lg"
+        className="main rounded-xl shadow-lg"
         />
      </div>
      </div>
 
      <div className="team-section mt-20">
-  <h2 className="text-3xl font-bold mb-10">Meet the SubDC Team</h2>
+  <h2 className="aboutus-title text-3xl font-bold mb-10">Meet the SubDC Team</h2>
   
   <div className="flex flex-wrap justify-center">
     {/* Team Members - make this an element later bc 2 much code */} 
+    <a href="https://linkedin.com/in/cdirubbio">
     <div className="team-member bg-white shadow-lg rounded-lg p-5 m-4 max-w-xs">
       <img 
         src={ChrisImg} 
         alt="Chris" 
         className="w-32 h-auto rounded-full mx-auto mb-4 object-cover"  
       />
-      <h3 className="text-xl font-semibold">CEO & Dev</h3>
+      <h3 className="text-xl font-semibold">CEO & Developer</h3>
       <p className="text-gray-600">Christian DiRubbio</p>
       <p className="text-gray-500 mt-3">
         
       </p>
     </div>
+    </a>
     <div className="team-member bg-white shadow-lg rounded-lg p-5 m-4 max-w-xs">
       <img 
         src={SimoImg} 
